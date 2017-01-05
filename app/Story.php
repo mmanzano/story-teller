732f845112fbf34c559384354b8dcb6fdf798d8c
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2017-01-05 21:06:06 +0100

    Relationships

--- app/Story.php
+++ app/Story.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Story extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
