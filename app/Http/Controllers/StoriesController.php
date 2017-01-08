3ef735ac72e9cb0bb13a334fb71ea5351462954d
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2017-01-08 21:59:27 +0100

    Stories show as public

--- app/Http/Controllers/StoriesController.php
+++ app/Http/Controllers/StoriesController.php
@@ -9,7 +9,7 @@ class StoriesController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except(['index']);
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     /**
